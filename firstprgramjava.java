2c24cb30b190b320c605c68128a67df7db4a43b0
shrikantkmanwatkar <106794695+shrikantkmanwatkar@users.noreply.github.com>
2022-06-09 18:17:10 +0530

    demo

---
+++ firstprgramjava.java
@@ -0,0 +1,12 @@
+package corejava;
+
+public class firstprgramjava {
+
+	public static void main(String[] args) {
+	// to print the output, below statement used.
+		System.out.println("hello shrikant");
+System.out.println("jai mata di");
+
+	}
+
+}
